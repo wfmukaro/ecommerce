4837edb0ec1744cc43be8dc7d0b4856d5ad91175
Walter Mukaro <wfmukaro@yahoo.com>
2021-02-07 07:16:24 +0200

    first commit setting up the project

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+//require("dotenv").config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(PORT, () => console.log(`server running on port ${PORT}!`))
