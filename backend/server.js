86fe68b292be72e0a6ff7d6eabbc89c4d591c330
Walter Mukaro <wfmukaro@yahoo.com>
2021-02-07 18:36:46 +0200

    backend is done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 //require("dotenv").config()
 const express = require('express');
 const connectDB = require('./config/db');
-const app = express()
+const productRoutes = require('./routes/productRoutes')
+const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 connectDB();
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/api/products',productRoutes)
 app.listen(PORT, () => console.log(`server running on port ${PORT}!`))
