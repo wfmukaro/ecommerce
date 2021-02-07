56e4a0499e784cb13caca3607fd7299002bc4b7a
Walter Mukaro <wfmukaro@yahoo.com>
2021-02-07 17:39:38 +0200

    load data to the database

---
+++ backend/seederScript.js
@@ -0,0 +1,17 @@
+const productsData=require('./data/products');
+const connectDB=require('./config/db');
+const Product =require('./models/Product');
+connectDB();
+const importData= async()=>{
+    try {
+        await Product.deleteMany({});
+        await Product.insertMany(productsData)
+        console.log("Data import Sucess")
+        process.exit()
+    } catch (error) {
+        console.error("Error with data import")
+        process.exit(1)
+        
+    }
+}
+importData();
