33572285b64990a3ac2e3d6b20546bed9fc3222c
Walter Mukaro <wfmukaro@yahoo.com>
2021-02-09 14:58:17 +0200

    format cartscreem ---All formatting is done. Next add redux for state management

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,11 +1,25 @@
+import CartItem from '../components/CartItem'
 import './CartScreen.css'
 
 const CartScreen = () => {
     return (
         <div className="cartscreen">
-            Cart Screen
+           <div className="cartscreen__left">
+               <h2>Shopping Cart</h2>
+               <CartItem/>
+           </div>
+           <div className="cartscreen__right">
+                <div className="cartscreen__info">
+                    <p>Subtotal (0) items</p>
+                    <p>R499.99</p>
+                </div>
+                <div>
+                    <button>Proceed To Checkout</button>
+                </div>
+           </div>
         </div>
     )
 }
+
 
 export default CartScreen
